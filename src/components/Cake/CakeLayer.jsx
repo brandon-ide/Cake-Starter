418c8bcee8b2c7cc24c2fa47e565a6848f962141
kwright73 <kayceewright@gmail.com>
2024-12-09 18:56:05 -0500

    Cake layer changes

--- src/components/Cake/CakeLayer.jsx
+++ src/components/Cake/CakeLayer.jsx
@@ -8,10 +8,20 @@ const CakeLayer = ({ layer }) => {
       className="cake-layer"
       style={{
         backgroundColor: color,
-        width: `${width * 10}px`,
-        height: `${height * 10}px`,
+        width: `${width * 40}px`,
+        height: `${height * 20}px`,
+
       }}
-    ></div>
+      
+    >
+      <div className="top"
+      style={{
+        backgroundColor: color,
+        width: `${width * 39.5}px`,
+        height: `${height * 3}px`,
+
+      }}></div>
+    </div>
   );
 };
 
